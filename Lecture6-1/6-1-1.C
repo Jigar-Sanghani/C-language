ae192bb1b145e38584a79aeeea478454b51a230d
Jigar-Sanghani <149694591+Jigar-Sanghani@users.noreply.github.com>
2023-12-13 09:57:07 +0530

    Lecture6-1

---
+++ Lecture6-1/6-1-1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int J = 1;
+	clrscr();
+	while(J <= 10)
+	{
+		printf("%d   ",J);
+		J ++;
+	}
+	getch();
+}
---
+++ Lecture6-1/6-1-2.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int J = 10
+	clrscr();
+
+	while(J>=1)
+	{
+		printf("%d",J);
+		J--;
+	}
+	getch();
+}
